a61e2eb5f954e1f31007a0e9480dda1e97a2c467
Poojith69 <139998240+Poojith69@users.noreply.github.com>
2023-07-19 21:44:01 +0530

    Add files via upload

---
+++ eCommerce-main1/eCommerce/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.contrib import admin
+from store import views
+
+urlpatterns = [
+        path('', views.store, name="store"),
+        path('admin/',admin.site.urls),
+        path('store/', views.store, name="store"),
+        path('cart/', views.cart, name="cart"),
+        path('checkout/', views.checkout, name="checkout"),
+        path('update_item/', views.updateItem, name="update_item"),
+        path('process_order/', views.processOrder, name="process_order"),
+        path('login/', views.login_view, name="login"),
+        path('logout/', views.logout_view, name="logout"),
+        path('signup/', views.signup_view, name="signup"),
+]
---
+++ eCommerce-main1/store/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+class UserRegistrationForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password and confirm_password and password != confirm_password:
+            raise ValidationError("Passwords do not match.")
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("This username is already taken.")
+        return username
